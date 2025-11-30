9e3e4ad5ce21612ce83399bfcfad4771e265f486
이원빈 <ldcumg@naver.com>
2025-12-01 04:25:31 +0900

    Apply linearized history

--- types.d.ts
+++
@@ -1,9 +0,0 @@
-interface BeforeInstallPromptEvent extends Event {
-  readonly platforms: string[];
-  prompt(): Promise<void>;
-  userChoice: Promise<{ outcome: 'accepted' | 'dismissed'; platform: string }>;
-}
-
-interface WindowEventMap {
-  beforeinstallprompt: BeforeInstallPromptEvent;
-}

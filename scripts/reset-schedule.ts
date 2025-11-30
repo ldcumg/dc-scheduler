d537297a84863feb667dbc2353106618859675ea
이원빈 <ldcumg@naver.com>
2025-12-01 00:48:58 +0900

    chore: github action schedule 초기화

--- scripts/reset-schedule.ts
+++ scripts/reset-schedule.ts
@@ -1,17 +1,14 @@
 import admin from 'firebase-admin';
 import { Firebase } from '../src/constants';
 
-// 스크립트 시작 로그
-console.log('=== [Script] Start Reset Schedule ===');
-
-async function main() {
+(async () => {
   try {
     // 1. 환경변수 체크
     if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
       throw new Error('FIREBASE_SERVICE_ACCOUNT 환경변수가 없습니다.');
     }
 
-    // 2. 서비스 계정 키 디코딩 (GitHub Secret에 Base64로 넣었을 경우)
+    // 2. 서비스 계정 키 디코딩
     const serviceAccount = JSON.parse(
       Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString(
         'utf-8'
@@ -25,18 +22,11 @@ async function main() {
         'https://dongmun-work-schedule-default-rtdb.asia-southeast1.firebasedatabase.app',
     });
 
-    console.log('=== [Script] Deleting schedule... ===');
     const db = admin.database();
-
-    // 4. 데이터 삭제 (비동기 대기)
     await db.ref(Firebase.SCHEDULE).remove();
-
-    console.log('=== [Script] RTDB 초기화 성공! ===');
-    process.exit(0); // 성공 종료
+    process.exit(0);
   } catch (error) {
-    console.error('❌ [Script] 에러 발생:', error);
-    process.exit(1); // 실패 종료
+    console.error(error);
+    process.exit(1);
   }
-}
-
-main();
+})();
